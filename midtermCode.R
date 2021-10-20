104ca2770783f3ec6de0c1cefebc39c4274ee1c8
Polly Haun <h.polly@wustl.edu>
2021-10-20 12:15:29 -0500

    version1

---
+++ midtermCode.R
@@ -0,0 +1,32 @@
+#import data
+library(readr)
+library(tidyverse)
+brfss17 <- read_csv("https://raw.githubusercontent.com/kijohnson/ADA-Fall-2021/master/BRFSS2017_10percent_v.csv") %>%
+  na.omit()
+
+#summary stats
+summary(brfss17)
+
+#remove NA values from sex 
+brfss17$SEX <- factor(brfss17$SEX)
+levels(brfss17$SEX) <- c("male", "female", NA)
+table(brfss17$SEX)
+
+#subset by sex
+males<-brfss17[which(brfss17$SEX== "male"),]
+females <- brfss17[which(brfss17$SEX== "female"),]
+
+#find height median
+median(males$ht_meters, na.rm=TRUE)
+median(females$ht_meters, na.rm=TRUE)
+
+library(ggplot2)
+library(RColorBrewer)
+
+#create boxplot
+ggplot(data = na.omit(brfss17), aes(x = SEX, y = ht_meters))+
+  geom_boxplot() +
+  labs(x = "Sex", y="Heigh in meters", title = "Median Height Boxplot") +
+  scale_color_brewer(palette = "Accent")
+
+"C:\Users\polly\OneDrive\Fall 2021\ADA\midterm code.R"
